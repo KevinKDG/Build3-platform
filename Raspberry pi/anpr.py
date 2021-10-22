aeca1b0076434e26eff1873fb61b9c7a8040e278
KevinKDG <72076173+KevinKDG@users.noreply.github.com>
2021-10-22 10:01:03 +0200

    Update anpr.py

--- Raspberry pi/anpr.py
+++ Raspberry pi/anpr.py
@@ -6,9 +6,13 @@ from io import BytesIO
 from time import sleep
 from picamera import PiCamera
 from PIL import Image
+import mysql.connector
+from datetime import datetime
 
+conn_obj = mysql.connector.connect(host="localhost", user="admin", passwd="password", database="DB1")
 
 def main():
+ global conn_obj
  while True:
      camera = PiCamera()
      stream = BytesIO()
@@ -21,8 +25,7 @@ def main():
              'https://api.platerecognizer.com/v1/plate-reader/',
              #data=dict(regions=['us-ca'], config=json.dumps(dict(region="strict"))),  # Optional
              files=dict(upload=stream.read()),
-             headers={'Authorization': 'Token token hier in plaatsen'})
-             #voorbeeld token letterlijk met "Token" voor: 'Token 41c72...'
+             headers={'Authorization': 'Token 4632e5c9355e589155eb276c630d7d798fbc93bd'})
      response = response.json()
      #pprint(response)
      amount = response['results']
@@ -38,9 +41,17 @@ def main():
                      print("Accuracy:  " + str(score))
                      print("Plate: " + str(plate))
              count += 1
+
      camera.close()
      #camera.stop_preview()
      sleep(2)
+
+     mycursor = conn_obj.cursor()
+     sql = "INSERT INTO platform (id, nummerplaat) VALUES (%s, %s)"
+     val = (4, str(plate))
+     mycursor.execute(sql, val)
+     conn_obj.commit()
+     print("1 New record has been inserted in the Database")
 
 if __name__ == '__main__':  # run tests if called from command-line
     main()
