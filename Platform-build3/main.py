4a6cbc9c2b3babcd97e59b27c06e9b78b3531c54
KevinKDG <72076173+KevinKDG@users.noreply.github.com>
2021-10-24 13:25:16 +0200

    Update main.py

--- Platform-build3/main.py
+++ Platform-build3/main.py
@@ -9,26 +9,24 @@ import json
 from mqtt import MQTTClient
 
 # ========== Wifi or NB
-wifi.wificonnect('ssid', 'wachtwoord') # ssid , password
-
+wifi.wificonnect('WiFi-5.0-133B', '24652CC8A4') # ssid , password
+# nb.nbconnect()
 
 #  ========== values
-randomval = os.urandom(1)[0] # 0 - 255
+randomval = os.urandom(1)[0]s # 0 - 255
 plate = "1abc123"
 
+pack = {"plate": plate, "value": randomval} #Json format
+jsonObj = json.dumps(pack)
+data = jsonObj
 # ========== MQTT
 
-while True:
-    print("connect client")
-    client = MQTTClient("fipy", "192.168.1.2", port=1883)
-    client.connect()
-    pack = {"plate": plate, "value": randomval} #Json format
-    jsonObj = json.dumps(jsonvalues)
-    data = jsonObj
+client = MQTTClient("fipy", "192.168.1.2", port=1883)
+client.connect()
+print("client connected")
 
+while True:
     client.publish(topic="fipy/data",msg=data)
     print("Value has been sent!")
     print(data)
     time.sleep(10)
-
-lte.deinit()
